2ecd17c5de8cea9dbd7e382c3fb41933c3da2c19
aglegaspi <lex83nyc@gmail.com>
2019-08-13 15:42:03 -0400

    created tableview and model for custom cell

--- AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/ViewController.swift
+++ AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/ViewController.swift
@@ -8,16 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let zooAnimals = ZooAnimal.zooAnimals
+    
+    @IBOutlet weak var animalTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return zooAnimals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell = UITableViewCell()
+        
+        return cell
+    }
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        animalTableView.dataSource = self
+        animalTableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
 
---
+++ AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/animalTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  animalTableViewCell.swift
+//  AC-iOS-Multiple-MVC
+//
+//  Created by Alexander George Legaspi on 8/13/19.
+//  Copyright © 2019 AC-iOS. All rights reserved.
+//
+
+import UIKit
+
+class animalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
